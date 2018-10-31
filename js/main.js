ffffbaf44f688892203186476952c40e02d089e7
romaniaa <roman@romanhermens.com>
2018-10-31 16:23:46 -0600

    Edits to the overall aesthetic
    Getting "bottom-up" navigation and adding some more pizzazz.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 
 
   function openNav() {
-      document.getElementById("mySidenav").style.width = "90%";
-      document.getElementById("main_nav").style.marginLeft = "90%";
+      document.getElementById("mySidenav").style.height = "100%";
+      document.getElementById("main_nav").style.marginTop = "100%";
       document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
 
   function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main_nav").style.marginLeft= "0";
+      document.getElementById("mySidenav").style.height = "0";
+      document.getElementById("main_nav").style.marginTop= "0";
       document.body.style.backgroundColor = "black";
   }
