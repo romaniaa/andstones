a9c974d42d859009e410661307433f5b048f6912
romaniaa <roman@romanhermens.com>
2018-11-09 14:11:23 -0700

    Changing the look a bit,

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 
-
   function openNav() {
-      document.getElementById("mySidenav").style.height = "100%";
-      document.getElementById("main_nav").style.marginTop = "100%";
-      document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main_nav").style.marginLeft= "0";
+      document.body.style.backgroundColor = "black";
   }
 
   function closeNav() {
-      document.getElementById("mySidenav").style.height = "0";
-      document.getElementById("main_nav").style.marginTop= "0";
-      document.body.style.backgroundColor = "black";
+    if (x.matches) { // If media query matches
+            document.getElementById("mySidenav").style.width = "90%";
+        } else {
+            document.getElementById("mySidenav").style.width = "50%";
+        }
+      document.getElementById("main_nav").style.marginLeft = "-30px";
+      document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
   }
+
+  var x = window.matchMedia("(max-width: 900px)")
+  openNav(x) // Call listener function at run time
+  x.addListener(openNav) // Attach listener function on state changes
